7e1cbd810925941172e948752fc36eb5a7db4bb1
sergio-lima <sergiodelima22@gmail.com>
2024-03-11 15:21:45 -0300

    Refatoracao

--- pega-cotacao-com-api/js/scripts.js
+++ pega-cotacao-com-api/js/scripts.js
@@ -1,5 +1,5 @@
 // Seleção de Elementos:
-var radioButton = document.querySelectorAll(".radio");
+var caixaDeSelecao = document.querySelector("#moedas");
 var botao = document.querySelector('#button');
 radioButton[2].attributes['id'].value;
 
@@ -7,9 +7,8 @@ radioButton[2].attributes['id'].value;
 botao.addEventListener("click",  async function pegarCotacaoMoeda(moedaDesejada) {
     const pegandoCotacao = await fetch("https://economia.awesomeapi.com.br/last/" + moedaDesejada);
     const convertendoDadosEmJson = await pegandoCotacao.json(); // Agora estamos esperando a promessa ser resolvida
-
+    alert(caixaDeSelecao)
 })
-
 
 
 // Moedas selecionadas separado por vírgula (,) 
