f87007cffc60932b078e777d13b70887a8d2e90e
sergio-lima <sergiodelima22@gmail.com>
2024-03-12 15:03:23 -0300

    Construcao da Estrutura HTML

---
+++ calculadora-imc/js/scripts.js
@@ -0,0 +1,9 @@
+// Seleção de Elementos:
+var nome = document.querySelector('#nome');
+var altura = document.querySelector("#altura");
+var peso = document.querySelector("#peso");
+var botao = document.querySelector("#botao");
+
+botao.addEventListener("click", function calcularImc() {
+    alert('Funciona!')
+})
