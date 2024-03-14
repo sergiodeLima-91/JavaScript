c0401fbd922a3ccf0d19c944ed3639efa48ce8c1
sergio-lima <sergiodelima22@gmail.com>
2024-03-14 10:49:18 -0300

    Refatoracao

--- calculadora-imc/js/scripts.js
+++ calculadora-imc/js/scripts.js
@@ -2,8 +2,9 @@
 var nome = document.querySelector('#nome');
 var altura = document.querySelector("#altura");
 var peso = document.querySelector("#peso");
+var resultado = document.querySelector("#results")
 var botao = document.querySelector("#botao");
 
-botao.addEventListener("click", function calcularImc() {
-    alert('Funciona!')
+botao.addEventListener("click", function() {
+    alert((altura.value ** 2) / peso.value)
 })
