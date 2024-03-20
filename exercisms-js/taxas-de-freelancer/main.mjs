47e3aeb20ff3718112227336238252d12a023ef9
sergio-lima <sergiodelima22@gmail.com>
2024-03-20 15:13:42 -0300

    Exercise Done!

--- exercisms-js/taxas-de-freelancer/main.mjs
+++ exercisms-js/taxas-de-freelancer/main.mjs
@@ -10,6 +10,31 @@
 // 3 - CALCULE A TAXA DE DESCONTOS PARA GRANDES PROJETOS:
 // Freqüentemente, os clientes do freelancer os contratam para projetos que duram vários meses. Nestes casos, o freelancer decide oferecer um desconto para cada mês completo, e os dias restantes são cobrados à diária. Cada mês tem 22 dias faturáveis. Ajude-os a estimar o custo desses projetos, considerando uma taxa horária, o número de dias que o projeto dura e uma taxa de desconto mensal. O desconto é sempre passado como um número, onde 42% passa a ser 0.42. O resultado deve ser arredondado para o número inteiro mais próximo.
 
+
+// // Taxa de Descontos para grandes projetos:
+// export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
+//     // Calculo para Determinar quantidade de meses de 22 dias:
+//     var monthNum = Math.round(numDays / 22);
+//     // Determinando quanto será o valor pago no mês (sem desconto)
+//     var monthValue = (ratePerHour * 8) * 22;
+//     // Determinando a porcentagem de desconto com base no montante:
+//     var monthValueDiscount = (monthValue * (discount * 100)) / 100;
+//     // Aplicando o desconto determinado no montante:
+//     var valueMonthly = (monthValue - monthValueDiscount) * monthNum ;
+    
+//     var restDays = numDays % 22;
+//     var daysRemainigValue = (ratePerHour * 8) * restDays;
+    
+//     return `monthNum: ${monthNum} - monthValue: ${monthValue} - monthValueDiscount: ${monthValueDiscount} - restDays: ${restDays} - daysRemainingValue: ${daysRemainigValue} - total: ${valueMonthly + daysRemainigValue}`; 
+// }
+
+// // Chamamento das funções:
+// dayRate();
+// daysInBudget();
+// priceWithMonthlyDiscount(16, 130, 0.15);
+
+//==============================================================
+
 // Valor Diário com Base no Valor Por Hora:
 export function dayRate(ratePerHour) {
     return ratePerHour * 8;
@@ -17,16 +42,29 @@ export function dayRate(ratePerHour) {
 
 // Número de Dias Úteis com Orçamento Fixo:
 export function daysInBudget(budget, ratePerHour) {
-    return Math.round((budget / ratePerHour) / 8);
+    return Math.floor((budget / ratePerHour) / 8);
 };
+
 
 // Taxa de Descontos para grandes projetos:
 export function priceWithMonthlyDiscount(ratePerHour, numDays, discount) {
-    var calcMonth = (numDays / 30).toFixed(0);
-    return Math.round((calcMonth * ratePerHour));
+    // Calculo para Determinar quantidade de meses de 22 dias:
+    var monthNum = Math.floor(numDays / 22); // Correção: usar Math.floor para arredondar para baixo
+    // Determinando quanto será o valor pago no mês (sem desconto)
+    var monthValue = (ratePerHour * 8) * 22;
+    // Determinando a porcentagem de desconto com base no montante:
+    var monthValueDiscount = (monthValue * (discount * 100)) / 100;
+    // Aplicando o desconto determinado no montante:
+    var valueMonthly = (monthValue - monthValueDiscount) * monthNum;
+    
+    var restDays = numDays % 22;
+    var daysRemainingValue = (ratePerHour * 8) * restDays;
+
+
+    return Math.ceil(valueMonthly + daysRemainingValue);
 }
 
 // Chamamento das funções:
 dayRate();
 daysInBudget();
-console.log(priceWithMonthlyDiscount(89, 230, 0.42)); 
+console.log(priceWithMonthlyDiscount(29.654321, 155, 0.2547));
