9111bc99e000b3fda9ed39880bec5d20c75c9bfe
sergio-lima <sergiodelima22@gmail.com>
2024-03-21 10:45:16 -0300

    First Commit

---
+++ exercisms-js/poetry-club-door-policy/main.mjs
@@ -0,0 +1,16 @@
+// Capture of first charactere:
+export function frontDoorResponse(verse) {
+    var fisrtCharList = [];
+    fisrtCharList = verse[0];    
+    return fisrtCharList;
+};
+
+// Captalization of fisrt chacarter only:
+export function frontDoorPassword(verse) {
+    return verse.replace(/^(.)(.*)/, _, )
+}
+let char1 = frontDoorResponse('Stands so high')
+let char2 = frontDoorResponse('Huge hooves too')
+let char3 = frontDoorResponse('Impatiently waits for')
+let char4 = frontDoorResponse('Reins and harness')
+let char5 = frontDoorResponse('Eager to leave')
