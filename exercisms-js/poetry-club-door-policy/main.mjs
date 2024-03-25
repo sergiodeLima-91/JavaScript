bef189e6a0625d1810e205b943311eb50c5e89c4
sergio-lima <sergiodelima22@gmail.com>
2024-03-25 10:45:46 -0300

    TO DO: Trying transform array to complete string for captalize the password

--- exercisms-js/poetry-club-door-policy/main.mjs
+++ exercisms-js/poetry-club-door-policy/main.mjs
@@ -1,16 +1,29 @@
-// Capture of first charactere:
-export function frontDoorResponse(verse) {
-    var fisrtCharList = [];
-    fisrtCharList = verse[0];    
-    return fisrtCharList;
+// Main Function:
+export function frontDoorResponse(line) {
+    const poetry = line;
+    // Catching the first letter of each word :
+    const firstLetters = poetry.map(function(item, index) {
+        return item[0];
+    });
+
+    // Putting letters togheter to form the password:
+    var lettersTogheter = '';
+    firstLetters.forEach(function(item){
+        return lettersTogheter = item;
+    });
+    // Captalize the passowrd:
+    // const captalizedPassword = firstLetters.charAt(0).toUpperCase() + firstLetters.slice(1).toLowerCase();
+
+    return lettersTogheter;
 };
 
-// Captalization of fisrt chacarter only:
-export function frontDoorPassword(verse) {
-    return verse.replace(/^(.)(.*)/, _, )
-}
-let char1 = frontDoorResponse('Stands so high')
-let char2 = frontDoorResponse('Huge hooves too')
-let char3 = frontDoorResponse('Impatiently waits for')
-let char4 = frontDoorResponse('Reins and harness')
-let char5 = frontDoorResponse('Eager to leave')
+var SUMMER = [
+    'Sunshine warming my toes,',
+    'Underwater fun with my friends.',
+    'Making homemade ice cream on the porch,',
+    'Many long nights catching fireflies.',
+    'Early morning walks to the creek,',
+    'Reveling in the freedom of lazy days.',
+];
+
+console.log(frontDoorResponse(SUMMER));
