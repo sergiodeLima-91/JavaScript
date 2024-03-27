8851b6e1951ff7984f05af4a40c949d01c7bde42
sergio-lima <sergiodelima22@gmail.com>
2024-03-27 10:58:06 -0300

    Refactoring

--- exercisms-js/poetry-club-door-policy/main.mjs
+++ exercisms-js/poetry-club-door-policy/main.mjs
@@ -1,26 +1,51 @@
-// Main Function:
+// Cathching the first letter:
 export function frontDoorResponse(line) {
     const poetry = line;
     // Catching the first letter of each word :
-    const firstLetters = poetry.map(function(item) {
-        return item[0];
+    const firstLetters = poetry.map(function(first_letter, index) {
+        return first_letter[0];
     });
     // Putting letters togheter to form the password:
-    var lettersTogheter = firstLetters.join('');
+    const lettersTogheter = firstLetters.join('');
+
+    return lettersTogheter;
+};
+// Catching the last letter:
+export function backDoorResponse(verse) {
+    const poetry = verse;
+    const lastLetters = poetry.map(function(last_letter){
+        // Remove commas, full points, empty spaces and tabs:
+        const cleanedVerse = last_letter.replace(/[,.\s]/g, "");
+        // Catch the last letter after clear the verse:
+        const last = cleanedVerse.charAt(cleanedVerse.length - 1);
+
+        return last;
+    });
+
+    const lettersTogheter = lastLetters.join('');
 
     return lettersTogheter;
 };
 
-   
+// Assembling functions that forma passwords:   
 export function frontDoorPassword() {
    // Captalize the passowrd:
-   const returnfrontDoorResponse = frontDoorResponse(SUMMER)
-   const captalizedPassword = returnfrontDoorResponse.charAt(0).toUpperCase() + returnfrontDoorResponse.slice(1).toLowerCase();
+   const returnFrontDoorResponse = frontDoorResponse(SUMMER);
+   const captalizedPassword = returnFrontDoorResponse.charAt(0).toUpperCase() + returnFrontDoorResponse.slice(1).toLowerCase();
 
    return captalizedPassword;
-}   
+}
 
-var SUMMER = [
+export function backDoorPassword() {
+    // Catching the return of backDoorResponse function:
+    const returnBackDoorResponse = backDoorResponse(HORSE);
+    // Capitalize the string captured:
+    const capitalizedPassword = returnBackDoorResponse.charAt(0).toUpperCase() + returnBackDoorResponse.slice(1).toLowerCase();
+    
+    return capitalizedPassword;
+}
+
+const SUMMER = [
     'Sunshine warming my toes',
     'Underwater fun with my friends.',
     'Making homemade ice cream on the porch,',
@@ -29,5 +54,18 @@ var SUMMER = [
     'Reveling in the freedom of lazy days.',
 ];
 
-console.log(frontDoorResponse(SUMMER));
+const HORSE = [
+"Stands so high",
+"Huge hooves too",
+"Impatiently waits for",
+"Reins and harness",
+"Eager to leave",
+]
+// Call Functions:
+frontDoorResponse(SUMMER);
 console.log(frontDoorPassword());
+
+backDoorResponse(HORSE);
+console.log(backDoorPassword());
+
+
