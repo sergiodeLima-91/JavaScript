06ccd2cab39bbdd4887ab4910e1812b33533a622
sergio-lima <sergiodelima22@gmail.com>
2024-03-28 10:29:22 -0300

    First Commit

---
+++ exercisms-js/annalyn-infiltration/main.mjs
@@ -0,0 +1,9 @@
+// Helping a friend who will develop an RPG game based on a choice system.
+
+export function canExecuteFastAttack(knightIsAwake) {
+    if (knightIsAwake == true) {
+        return false;
+    } else {
+        return true;
+    }
+};
