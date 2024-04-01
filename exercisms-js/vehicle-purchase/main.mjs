83492740d19adb7a183275f9675ca9d6ea73a23d
sergio-lima <sergiodelima22@gmail.com>
2024-04-01 11:14:26 -0300

    First commit

---
+++ exercisms-js/vehicle-purchase/main.mjs
@@ -0,0 +1,27 @@
+export function needsLicense(kind) {
+    if (kind == 'car' || kind ==  'truck') {
+        return true;
+    } else {
+        return false;
+    }
+};
+
+const alpha = [
+    a = 'A',
+    b = 'B',
+    c = 'C'.
+]
+
+export function chooseVehicle(option1, option2) {
+    if(option1[0] > option1[0]) {
+        return  `${option1} is clearly the better choice.`
+    } else{
+        return `${option2} is clearly the better choice.`
+    }
+
+};
+
+
+console.log(chooseVehicle('Wuling Hongguang', 'Toyota Corolla'));
+
+
