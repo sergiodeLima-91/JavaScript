2b910094732217123d718aba5a10b5f7b4f74440
sergio-lima <sergiodelima22@gmail.com>
2024-04-10 10:17:42 -0300

    Tasks 4 and 5 done - Update REAMDE

--- exercisms-js/amusement-park/main.mjs
+++ exercisms-js/amusement-park/main.mjs
@@ -16,18 +16,49 @@ export function revokeTicket(visitor) {
 
 export function ticketStatus(tickets, ticketId) {
     for (const key in tickets) {
+        // 1 - If the ticket is not in the object:
         if (tickets.hasOwnProperty(ticketId) == false) {
             return 'unknown ticket id';
+        // 2 - If the ticket has a value of “null”:
         } else if (tickets[ticketId] === null) {
             return 'not sold';
+        // 3 - If the ticket has a name as its value:
         } else {
             return `sold to ${tickets[ticketId]}`
         }
     }
 };
 
-const tickets = {
-    'V42NWRMQ': null,
+export function simpleTicketStatus(tickets, ticketId) {
+    for (const key in tickets) {
+        // 1 - If the ticket is not in the object or if the ticket has a value of “null”:
+        if (tickets.hasOwnProperty(ticketId) == false || tickets[ticketId] === null) {
+            return 'invalid ticket !!!';
+        // 2 - If the ticket has a name as its value:
+        } else {
+            return `${tickets[ticketId]}`
+        }
+    }
 };
 
-console.log(ticketStatus(tickets, 'V42NWRMQ'));
+const visitorNew = {
+    name: 'Verena Nardi',
+    age: 45,
+    ticketId: 'H32AZ123',
+    gtc: {
+      signed: true,
+      version: '2.1',
+    },
+};
+
+const visitorOld = {
+    name: 'Verena Nardi',
+    age: 45,
+    ticketId: 'H32AZ123',
+  };
+
+export function gtcVersion(visitor) {
+    return visitor.gtc?.version;
+};
+
+console.log(gtcVersion(visitorOld));
