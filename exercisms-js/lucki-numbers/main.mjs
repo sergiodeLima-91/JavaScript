29b852d28effc9a3161431ed9de96032bfdad376
sergiolima <sergiodelima22@gmail.com>
2024-04-11 22:03:46 -0300

    Task 3

---
+++ exercisms-js/lucki-numbers/main.mjs
@@ -0,0 +1,35 @@
+export function twoSum(array1, array2) {
+    // 1 - Eliminating commas:
+    array1 = array1.join('');
+    array2 = array2.join('');
+    //2 - Conterting:
+    array1 = Number(array1);
+    array2 = Number(array2);
+    //3 - Sum numbers:
+    let sum = array1 + array2;
+
+    return sum;
+
+};
+
+export function luckyNumber(value) { 
+    //1 - Converting to string:
+    let valueString = String(value);
+    //2 - Reverting the numbers:
+    let valueReverse = [...valueString];
+    valueReverse = valueReverse.reverse().join('');
+    //3 - Comparing the numbers :
+    return valueString === valueReverse;
+};
+
+export function errorMessage(input) {
+    if (input == undefined || input == null) {
+        return 'Required field';
+    } else if (input == 0) {
+        return 'Must be a number besides 0';
+    } else {
+        return '';
+    }
+  }
+
+  console.log(errorMessage(''));
