39bc3787cc7f069c00e8822d08d07488ff90255b
sergio-lima <sergiodelima22@gmail.com>
2024-04-24 11:00:21 -0300

    Adicionando Componentes

--- react/meu-primeiro-projeto/src/App.js
+++ react/meu-primeiro-projeto/src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   const mentira = 'Programar é fácil';
+  const url = 'https://via.placeholder.com/150'
 
   function sum(a,b) {
     return a + b;
@@ -12,6 +14,8 @@ function App() {
       <h1>Mentiras da Vida</h1>
       <p>A mentira de hoje é: {mentira}</p>
       <p>Soma: {sum(10,5)}</p>
+      <img src={url} alt='Minha imagem'></img>
+      <HelloWorld />
     </div>
   );
 }
---
+++ react/meu-primeiro-projeto/src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase() {
+    return (
+        <div>
+            <p>Frase que fica encima!</p>
+        </div>
+    )
+};
+
+export default Frase
