4bda8e2fb8212ea4580b4dd74a1b6cb70a46943e
sergio-lima <sergiodelima22@gmail.com>
2024-04-25 09:05:14 -0300

    Usando PROPS no projeto

--- react/meu-primeiro-projeto/src/App.js
+++ react/meu-primeiro-projeto/src/App.js
@@ -1,21 +1,18 @@
 import './App.css';
-import HelloWorld from './components/HelloWorld';
+
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
-  const mentira = 'Programar é fácil';
-  const url = 'https://via.placeholder.com/150'
-
-  function sum(a,b) {
-    return a + b;
-  };
-
   return (
     <div className="App">
-      <h1>Mentiras da Vida</h1>
-      <p>A mentira de hoje é: {mentira}</p>
-      <p>Soma: {sum(10,5)}</p>
-      <img src={url} alt='Minha imagem'></img>
-      <HelloWorld />
+      <SayMyName nome="Sérgio" />
+      <Pessoa
+        foto="https://via.placeholder.com/150" 
+        nome="Sérgio"
+        idade="33"
+        profissao="Auxiliar Administrativo"
+      />
     </div>
   );
 }
