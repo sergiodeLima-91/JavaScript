e0d5f41ed47fbbeb4b55cb98aef1f2ea77d2b020
sergio-lima <sergiodelima22@gmail.com>
2024-05-03 15:10:52 -0300

    Task 1

---
+++ exercisms-js/windowing-system/main.js
@@ -0,0 +1,13 @@
+// Task 1:
+class Size {
+    constructor(width=80, height=60) {
+        this.width = width;
+        this.height = height;
+    }
+
+    resize(newWidth, newHeight) {
+        return newWidth
+    }
+}
+
+const size = new Size();
