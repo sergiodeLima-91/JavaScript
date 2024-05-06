48489ce9c4cdaf0bb3d72685b45b868925422b03
sergio-lima <sergiodelima22@gmail.com>
2024-05-06 08:37:40 -0300

    Finalizando condicionais no React

--- react/meu-primeiro-projeto/src/components/Condicional.js
+++ react/meu-primeiro-projeto/src/components/Condicional.js
@@ -1,20 +1,35 @@
 import { useState } from "react";
 
 function Condicional() {
+    const [email, setEmail] = useState()
+    const [userEmail, setUserEmail] = useState()
 
     function enviarEmail(e) {
         e.preventDefault()
-        alert('Testando')
+        setUserEmail(email)
+        console.log(userEmail)
     }
 
-    const [email, setEmail] = useState()
+    function limparEmail() {
+        setUserEmail('')
+    }
+
+    
 
     return (
         <div>
             <h2>Cadastre o seu e-mail:</h2>
             <form>
-                <input type="email" placeholder="Digite seu email"></input>
+                <input type="email" placeholder="Digite seu email" onChange={(e) => setEmail(e.target.value)}></input>
                 <button type="submit" onClick={enviarEmail}>Enviar e-mail</button>
+                {/* Se o email do usuário estiver preenchido execute o bloco abaixo. Sim, uma maneira bem
+                esquisita de declarar condicionais no React! */}
+                {userEmail && (
+                    <div>
+                        <p>O e-mail do usuário é {userEmail}</p>
+                        <button onClick={limparEmail}>Limpar E-mail</button>
+                    </div>
+                )}
             </form>
         </div>
     )
