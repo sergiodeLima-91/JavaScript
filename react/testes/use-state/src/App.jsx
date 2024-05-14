ddc42b2fa8c900ced74335d10f3a52f0a8eef4d1
sergio-lima <sergiodelima22@gmail.com>
2024-05-14 15:23:34 -0300

    useState - Previous Value e onChange

--- react/testes/use-state/src/App.jsx
+++ react/testes/use-state/src/App.jsx
@@ -2,17 +2,39 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [numero, setNumero] = useState(0)
+  const [numero, setNumero] = useState(0);
+  const [nome, setNome] = useState();
+  const [idade, setIdade] = useState();
 
   const contador = () => {
-    setNumero(alert('Olá!'));
+    // Cada operação é executada uma de cada vez por causa do "prev" (previous, valor anterior)
+    setNumero((prevNumero) => prevNumero + 1);
+    setNumero((prevNumero) => prevNumero + 1);
+    setNumero((prevNumero) => prevNumero + 2);
   };
+
 
   return (
     <div className='App'>
-        <h1>Testando Hook useState até Aprender!</h1>        
-        <p>Mudando Número: <strong>{numero}</strong></p>
+        <h1>Testando Hook useState até Aprender!</h1>
+
+        <p>Número: <strong>{numero}</strong></p>
         <button onClick={contador}>Mudar Número</button>
+
+        <p>Meu nome é <strong>{nome}</strong></p>
+        <input
+        type='text'
+        value={nome} 
+        onChange={(e) => setNome(e.target.value)}
+        placeholder='Digite seu nome'
+        />
+        <p>Minha idade é <strong>{idade}</strong> anos</p>
+        <input 
+          type='number'
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+          placeholder='Digite sua idade'
+        />
     </div>
   )
 }
